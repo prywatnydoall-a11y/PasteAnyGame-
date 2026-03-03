9c209faafdeb8820b53458603925c50cbfa7d2f1
prywatnydoall-a11y <prywatnydoall@gmail.com>
2026-03-03 06:57:43 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const https://discord.com/api/webhooks/1478144033514590381/3KgD1bTbiegoQpDZhKLeexuUicadHlbjM8CsK9KYraM9hY3E0hzDfo_oxy25p1_40Y8m = process.env.WEBHOOK;
+const DISCORD_WEBHOOK_URL = process.env.WEBHOOK;
 
 app.use(express.json());
 app.use(express.static("public"));
@@ -11,10 +11,10 @@ app.post("/api/send-to-discord", async (req, res) => {
   const { text } = req.body;
 
   if (!text) return res.status(400).json({ error: "No text provided" });
-  if (!https://discord.com/api/webhooks/1478144033514590381/3KgD1bTbiegoQpDZhKLeexuUicadHlbjM8CsK9KYraM9hY3E0hzDfo_oxy25p1_40Y8m) return res.status(500).json({ error: "Webhook not set" });
+  if (!DISCORD_WEBHOOK_URL) return res.status(500).json({ error: "Webhook not set" });
 
   try {
-    await fetch(https://discord.com/api/webhooks/1478144033514590381/3KgD1bTbiegoQpDZhKLeexuUicadHlbjM8CsK9KYraM9hY3E0hzDfo_oxy25p1_40Y8m, {
+    await fetch(DISCORD_WEBHOOK_URL, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ content: text })
